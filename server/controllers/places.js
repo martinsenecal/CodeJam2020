904197488704ce3626e4fbde5ce627e36b53f1e0
Martin Senécal <martinsenecal2000@gmail.com>
2020-11-14 12:48:21 -0500

    Add Parsing for API Maps

--- server/controllers/places.js
+++ server/controllers/places.js
@@ -1,16 +1,43 @@
 require('express-async-errors');
 
+const { request } = require('express');
 //Import Models
 const Restaurant = require('../models/Restaurant');
 const Restaurants = require('../models/Restaurants');
 
+//Google Maps API -> to be used with
 const getPlaces = async (req, res, next) => {
-  //How to see this api: http://localhost:5000/api/session
   try {
-    res.send('hello world');
+    const options = {
+      uri: `todo`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+      if (response.statusCode != 200) {
+        return res.status(404).json({ msg: 'Problem with the request' });
+      }
+
+      //Body will be the JSON containing all the data
+      const parsedJSON = JSON.parse(body);
+
+      //Parse JSON into DB for Restaurants
+      for (obj in parsedJSON.results) {
+        const newRestaurant = new Restaurants({
+          place_id: obj.place_id,
+          company: obj.name,
+        });
+
+        const resto = await newRestaurant.save();
+      }
+
+      //res.json(JSON.parse(body)); TBD
+    });
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error');
+    return res.status(500).json({ msg: 'Server Error' });
   }
 };
 
--- server/controllers/session.js
+++ server/controllers/session.js
@@ -1,10 +1,29 @@
+const Session = require('../models/Session');
+
 require('express-async-errors');
 
 const getSession = async (req, res, next) => {
-  res.send('Hello World!');
+  try {
+    res.send('hello world');
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
   //How to see this api: http://localhost:5000/api/session
 };
 
-const postSession = async (req, res, next) => {};
+const postSession = async (req, res, next) => {
+  try {
+    const newSession = new Session({
+      //default info here
+    });
+
+    const session = await newSession.save();
+    res.json(session);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+};
 
 module.exports = { getSession, postSession };
