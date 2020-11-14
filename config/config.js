0aeda7168c070469585b6534bb3ac054a73ac4a9
Martin Senécal <martinsenecal2000@gmail.com>
2020-11-13 21:02:53 -0500

    Initial Commit

---
+++ config/config.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+const {​​ PORT, MONGODB_URL, GOOGLE_API_KEY, HOME_PAGE_DOMAIN}​​ = process.env;
+
+const config = {​​ PORT, MONGO_URL, GOOGLE_API_KEY, HOME_PAGE_DOMAIN}
+
+module.exports = config;​​
