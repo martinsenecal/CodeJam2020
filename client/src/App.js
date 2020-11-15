135fe3bec3e6b0c58d07752639c765a026d51e6f
Lenoy Christy <lenoy.christy@mail.mcgill.ca>
2020-11-15 00:31:58 -0500

    Initial react setup stuff done, still need to fix modal shit

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
+import './Landing.css'
 import Landing from './components/layout/Landing';
+
+// Bootstrap Imports
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
